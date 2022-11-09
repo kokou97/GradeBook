317ae3d866495b9a3004d4c7ceaf521ab2c35db1
kokou97 <jdjadou@yahoo.fr>
2022-11-09 12:16:57 +0100

    ...

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,14 +8,21 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             var numbers = new[] { 12.7, 10.3, 6.11 };
-            var grades = new List<double>();
+            var grades = new List<double>() { 12.7, 10.3, 6.11 };
             grades.Add(56.1);
-            var result = 0.0;
+            var result1 = 0.0;
             foreach (var number in numbers)
             {
-                result += number;
+                result1 += number;
             }
-            Console.WriteLine(result);
+            var result2 = 0.0;
+            foreach (var grade in grades)
+            {
+                result2 += grade;
+            }
+            Console.WriteLine(result1);
+            Console.WriteLine(result2 + "\n");
+            Console.WriteLine(grades.Sum());
             if (args.Length < 1)
             {
                 Console.WriteLine("Hello, World!");
